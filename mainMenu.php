f6474a327f085d5e31672debcd306015cfb3b4a1
marwahkamilaahmad <mymarwahmad@gmail.com>
2024-10-22 09:41:23 +0700

    first set up

---
+++ mainMenu.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Main Menu</title>
+    <link href="style.css" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+</head>
+<body>
+  <style>
+    body {
+      background-image: url('wallpaper1.jpg');
+    }
+  </style>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+    <nav class="navbar navbar-expand-lg bg-body-tertiary">
+      <div class="container-fluid">
+        <div class="collapse navbar-collapse" id="navbarText">
+          <ul class="navbar-nav mb-2">
+            <li class="nav-item text-decoration-underline">
+              <a class="nav-link active" aria-current="page" href="#">Home</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Pilihan Beasiswa</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Daftar</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Hasil</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+
+
+    <div class="card mt-4 mx-4">
+      <h5 class="card-header">Beasiswa</h5>
+      <div class="card-body">
+        <h5 class="card-title">Pendaftaran Beasiswa Telah Dibuka</h5>
+        <p class="card-text">Untuk mendaftar beasiswa klik di bawah ini</p>
+        <a href="#" class="btn btn-info">Daftar</a>
+      </div>
+    </div>
+
+<!-- <div class="dropdown">
+  <button class="btn btn-primary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+    Dropdown
+  </button>
+  <ul class="dropdown-menu">
+    <li><button class="dropdown-item" type="button">Dropdown item</button></li>
+    <li><button class="dropdown-item" type="button">Dropdown item</button></li>
+    <li><button class="dropdown-item" type="button">Dropdown item</button></li>
+  </ul>
+</div> -->
+</body>
+</html>
